919f3ace12491f7c0864534db59d1b08c703bc30
iduvall <iduvall@umich.edu>
2017-02-23 00:11:37 -0500

    test.py tries to request all of the courses for every subject in every school for the current semester. Currently times out due to throttling when requesting Med School classes. May just need to add sleep statements to slow the program down.

---
+++ README.md
@@ -0,0 +1,7 @@
+# Prototype Code
+
+## test.py
+To run: python test.py
+Possible errors:
+	- authentication error: regenerate key, secret and token in api web app, copy new values to top of test.py
+	
--- test.py
+++ test.py
@@ -10,8 +10,11 @@ import requests
 # THIS STILL DOES NOT WORK
 # curl -k -d "grant_type=client_credentials&scope=PRODUCTION" -H "Authorization: Basic NnY2UGRoX0s0dW5oanZzSkh1SUlkcUtwRXJBYTpQUTBTRmVNT3pvbVFzU1hTQjBRenpGc213dW9h, Content-Type: application/x-www-form-urlencoded" https://api-km.it.umich.edu/token
 
-key = "uatEnJRH_EDyne8rbD9WEd4IH3Ma"
-secret = "skX2k6vTfrz8MHGtfwUdkIoRMRUa"
+key = "ojLHnLN3a6Ma6Rn_fk2niTFlr7Ea"
+secret = "YEqiqUlJtL4EQdSBhf_Sh43zrf0a"
+access_token = "439ce028ab2e8d4619c9223e9f8bec5"
+
+
 combined_key = key + ":" + secret
 print("combined key: " + combined_key)
 encoded_key = combined_key.encode('base64').strip()
@@ -40,7 +43,7 @@ term_code = ""
 base_url = "https://api-gw.it.umich.edu/Curriculum/SOC/v1"
 headers = {
 	"Accept": "application/json",
-	"Authorization": "Bearer 21755e9dfbf737935629d2bf9c39b"
+	"Authorization": "Bearer " + access_token
 }
 
 term_url = base_url + "/Terms"
@@ -57,20 +60,88 @@ else:
 
 
 # Requests all eecs classes
-eecs_classes_url = base_url + "/Terms/{}/Schools/{}/Subjects/{}/CatalogNbrs".format(term_code, "ENG", "EECS")
-eecs_classes = requests.get(eecs_classes_url, headers=headers)
-if eecs_classes.status_code == 200:
-	eecs_classes_json = eecs_classes.json()
-	# print eecs_classes_json
-	#print(responseJSON["getSOCTermsResponse"]["Term"]["TermDescr"])
-	#print(responseJSON["getSOCTermsResponse"]["Term"]["TermCode"])
+# eecs_classes_url = base_url + "/Terms/{}/Schools/{}/Subjects/{}/CatalogNbrs".format(term_code, "ENG", "EECS")
+# eecs_classes = requests.get(eecs_classes_url, headers=headers)
+# if eecs_classes.status_code == 200:
+# 	eecs_classes_json = eecs_classes.json()
 
-	for class_obj in eecs_classes_json["getSOCCtlgNbrsResponse"]["ClassOffered"]:
-		print("Catalog #: {}, Course Descr: {}".format(class_obj["CatalogNumber"], class_obj["CourseDescr"]))
+# 	for class_obj in eecs_classes_json["getSOCCtlgNbrsResponse"]["ClassOffered"]:
+# 		print("Catalog #: {}, Course Descr: {}".format(class_obj["CatalogNumber"], class_obj["CourseDescr"]))
 
-else:
-	print(eecs_classes.status_code)
-	print(eecs_classes.text)
+# else:
+# 	print(eecs_classes.status_code)
+# 	print(eecs_classes.text)
+# 	exit(1)
+
+
+
+def get_all_courses_for_subject(term_c, school_c, subject_c):
+	courses_url = base_url + "/Terms/{}/Schools/{}/Subjects/{}/CatalogNbrs".format(term_c, school_c, subject_c)
+	courses = requests.get(courses_url, headers=headers)
+	if courses.status_code != 200:
+		print(courses.status_code)
+		print(courses.text)
+		exit(1)
+
+	courses_json = courses.json()
+
+	if "getSOCCtlgNbrsResponse" not in courses_json or \
+	"ClassOffered" not in courses_json["getSOCCtlgNbrsResponse"]:
+		print("\t\t- Empty")
+		return
+		
+	# loop through classes
+	for course in courses_json["getSOCCtlgNbrsResponse"]["ClassOffered"]:
+		if type(course) is dict:
+			print("\t\t- {}".format(course["CatalogNumber"]))
+		else:
+			if course == "CatalogNumber":
+				print("\t\t- {}".format(courses_json["getSOCCtlgNbrsResponse"]["ClassOffered"][course]))
+
+	return # get_all_classes_for_subject
+
+
+# Requests all classes
+schools_url = base_url + "/Terms/{}/Schools".format(term_code)
+schools = requests.get(schools_url, headers=headers)
+if schools.status_code != 200:
+	print(schools.status_code)
+	print(schools.text)
 	exit(1)
+
+schools_json = schools.json()
+
+# loop through schools
+for school in schools_json["getSOCSchoolsResponse"]["School"]:
+	school_code = school["SchoolCode"]
+	print(school_code)
+
+	subjects_url = base_url + "/Terms/{}/Schools/{}/Subjects".format(term_code, school_code)
+	subjects = requests.get(subjects_url, headers=headers)
+	if subjects.status_code == 200:
+		subjects_json = subjects.json()
+
+		# loop through subjects
+		for subject in subjects_json["getSOCSubjectsResponse"]["Subject"]:
+			if school_code == "INF" or \
+			school_code == "LAW" or \
+			school_code == "NRE" or \
+			school_code == "IPP" or \
+			school_code == "SW":
+				if subject == "SubjectCode":
+					subject_code = subjects_json["getSOCSubjectsResponse"]["Subject"]["SubjectCode"]
+					print("\t- {}".format(subject_code))
+				else:
+					continue
+			else:
+				if "SubjectCode" in subject:
+					subject_code = subject["SubjectCode"]
+					print("\t- {}".format(subject_code))
+				else:
+					print("SubjectCode not in Subject")
+					continue
+
+			# gets all courses for subject code, prints for now
+			get_all_courses_for_subject(term_code, school_code, subject_code)
 
 
